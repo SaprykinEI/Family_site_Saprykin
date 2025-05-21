4fb794bb46f8223f783f86b3dfbaf2c369fb4c94
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-21 13:57:54 +0300

    1. Переписана админка через @admin.register

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email', 'pk')
+    list_filter = ('last_name',)
+    search_fields = ('first_name', 'last_name')
 
