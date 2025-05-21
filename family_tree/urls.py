1bbba83eca4df4600c71334c34a8a2d6395470b9
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-21 20:31:53 +0300

    1. Добавлен шаблон сайта 2. Добавлены CSS, JS

--- family_tree/urls.py
+++ family_tree/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from family_tree.apps import FamilyTreeConfig
 
-from family_tree.views import index
+from family_tree.views import index_view, persons_list_view
+from . import views
 
 app_name = FamilyTreeConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index_view, name='index'),
+    path('persons/', persons_list_view, name='persons'),
 ]
--- family_tree/views.py
+++ family_tree/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from family_tree.models import Person
 
 
-def index(request):
+def index_view(request):
     context = {
         'objects_list': Person.objects.all(),
         'title': "<h1>Сайт семьи - Сапрыкины</h1>"
     }
     return render(request, 'family_tree/index.html', context=context)
+
+
+def persons_list_view(request):
+    context = {
+        'objects_list': Person.objects.all,
+        'title': "Все члены семьи"
+    }
+    return render(request, 'family_tree/persons_list.html', context=context)
