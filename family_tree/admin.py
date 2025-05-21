31f6f1ea40e5ca5fcf0a37930b19110d5fb248b5
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-21 14:49:29 +0300

    1. Зарегистрировали модель Person в admin.py

--- family_tree/admin.py
+++ family_tree/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from family_tree.models import Person
 
-# Register your models here.
+@admin.register(Person)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'pk')
+    list_filter = ('last_name',)
+    search_fields = ('first_name', 'last_name')
+    ordering = ('last_name', 'first_name')
