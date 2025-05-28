a02d6c91bb5810878d7a19e364b0025f22f3ab76
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-28 11:11:31 +0300

    1. Добавлена Форма регистрации пользователя 2. Написан шаблон регистрации пользователя 3. Написаны стили для формы регистрации

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from users.models import User
+
+
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'password', 'password2'
+
+    def clean_password2(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['password'] != cleaned_data['password2']:
+            raise forms.ValidationError("Пароли не совпадают!")
+        return cleaned_data['password2']
