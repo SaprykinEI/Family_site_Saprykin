bca5407cc27be2f771aa95f88efb439894e8c110
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-30 10:09:17 +0300

    1. Добавлено поле телефон в модель users 2. Проведены миграции

--- users/models.py
+++ users/models.py
@@ -7,8 +7,9 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
-    telegram = models.CharField(max_length=150, verbose_name="Telegram username", **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name="Active")
+    phone = models.CharField(max_length=20, verbose_name="Телефон", **NULLABLE)
+    telegram = models.CharField(max_length=150, verbose_name="Телеграм", **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name="Активность")
 
     # Communication with a Person (one-to-one)
     person = models.OneToOneField('family_tree.Person', on_delete=models.SET_NULL, **NULLABLE, related_name='user')
