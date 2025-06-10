cb2a959cd901416cfc13f8a98d316ba885c3ad04
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-10 10:18:49 +0300

    1. В приложение gallery прописаны модели Tag, Category, Album, Photo, Video 2. Созданы и проведены миграции

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from family_tree.models import Person
+from users.models import NULLABLE
+
+from gallery.utils import photo_upload_path, video_upload_path
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Тег")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name="Название категории")
+    description = models.TextField(**NULLABLE, verbose_name="Описание")
+    color = models.CharField(max_length=20, **NULLABLE, verbose_name="Цвет")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название альбома")
+    date = models.DateField(verbose_name="Дата")
+    description = models.TextField(**NULLABLE, verbose_name="Описание альбома")
+    cover_image = models.ImageField(upload_to='album_covers/', verbose_name="Обложка альбома")
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, **NULLABLE,
+                                 related_name='albums', verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Альбом"
+        verbose_name_plural = "Альбомы"
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to=photo_upload_path, verbose_name="Фотография")
+    caption = models.CharField(max_length=255, **NULLABLE, verbose_name="Подпись")
+    date_taken = models.DateField(**NULLABLE, verbose_name="Дата")
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='photos', verbose_name="Привязка к альбому")
+    people = models.ManyToManyField('family_tree.Person', **NULLABLE,
+                                    related_name='photos', verbose_name="Привязка к карточке")
+    tags = models.ManyToManyField('Tag', **NULLABLE, related_name='photos', verbose_name="Привязка к тегам")
+
+    def __str__(self):
+        return self.caption or f"Фото от {self.date_taken}"
+
+
+class Video(models.Model):
+    file = models.FileField(upload_to=video_upload_path, **NULLABLE, verbose_name="Видео")
+    url = models.URLField(**NULLABLE, verbose_name="Ссылка на видео")
+    caption = models.CharField(max_length=255, **NULLABLE, verbose_name="Подпись")
+    date_taken = models.DateField(**NULLABLE, verbose_name="Дата")
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='videos', verbose_name="Привязка к альбому")
+    people = models.ManyToManyField('family_tree.Person', **NULLABLE,
+                                    related_name='videos', verbose_name="Привязка к карточке")
+    tags = models.ManyToManyField('Tag', **NULLABLE, related_name='videos', verbose_name="Привязка к тегам")
+
+    def __str__(self):
+        return self.caption or f"Видео от {self.date_taken}"
