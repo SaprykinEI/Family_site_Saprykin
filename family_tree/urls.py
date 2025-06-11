01a5c4c4f826e9e7439ab20804db62bd86062731
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-11 10:48:00 +0300

    1. Переписаны представления FBV на CBV

--- family_tree/urls.py
+++ family_tree/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path
 from family_tree.apps import FamilyTreeConfig
 
-from family_tree.views import index_view, persons_list_view, person_create_view, person_detail_view, person_update_view, \
-    person_delete_view, tree_view, tree_data_view, spouse_tree_data
+from family_tree.views import IndexView, PersonsListView, PersonCreateView, PersonDetailView, PersonUpdateView, \
+    PersonDeleteView, TreeView, TreeDataView, SpouseTreeDataView
 app_name = 'family_tree'
 
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('persons/', persons_list_view, name='persons'),
-    path('persons/create/', person_create_view, name='person_create'),
-    path('persons/detail/<int:pk>/', person_detail_view, name='person_detail'),
-    path('persons/update/<int:pk>/', person_update_view, name='person_update'),
-    path('persons/delete/<int:pk>/', person_delete_view, name='person_delete'),
+    path('', IndexView.as_view(), name='index'),
+    path('persons/', PersonsListView.as_view(), name='persons'),
+    path('persons/create/', PersonCreateView.as_view(), name='person_create'),
+    path('persons/detail/<int:pk>/', PersonDetailView.as_view(), name='person_detail'),
+    path('persons/update/<int:pk>/', PersonUpdateView.as_view(), name='person_update'),
+    path('persons/delete/<int:pk>/', PersonDeleteView.as_view(), name='person_delete'),
 
-    path('tree/', tree_view, name='tree' ),
-    path('tree/data/<int:person_id>/', tree_data_view, name='tree_data'),
-    path('tree/data/spouse/<int:spouse_id>/', spouse_tree_data, name='spouse_tree_data'),
-
-
+    path('tree/', TreeView.as_view(), name='tree' ),
+    path('tree/data/<int:person_id>/', TreeDataView.as_view(), name='tree_data'),
+    path('tree/data/spouse/<int:spouse_id>/', SpouseTreeDataView.as_view(), name='spouse_tree_data'),
 ]
--- family_tree/views.py
+++ family_tree/views.py
@@ -1,153 +1,183 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+
 
 from family_tree.models import Person
 from family_tree.forms import PersonForm
 
 
-def index_view(request):
-    context = {
-        'objects_list': Person.objects.all(),
-        'title': "<h1>Сайт семьи - Сапрыкины</h1>"
-    }
-    return render(request, 'family_tree/index.html', context=context)
+class IndexView(ListView):
+    '''Представление для отображения главной страницы'''
+    model = Person
+    template_name = 'family_tree/index.html'
+    context_object_name = 'person_list'
+
+    def get_context_data(self, **kwargs):
+        '''Переопределяем метод, чтобы добавить дополнительный контекст в шаблон.'''
+        context = super().get_context_data(**kwargs)
+        context['title'] = "<h1>Сайт семьи - Сапрыкины</h1>"
+        return context
 
 
-def persons_list_view(request):
-    context = {
-        'persons': Person.objects.all(),
-        'title': "Все члены семьи"
-    }
-    return render(request, 'family_tree/persons.html', context=context)
+class PersonsListView(ListView):
+    '''Представление-класс для отображения всех членов семьи.'''
+    model = Person
+    template_name = 'family_tree/persons.html'
+    context_object_name = 'persons'
+
+    def get_context_data(self, **kwargs):
+        ''' Добавляем в контекст дополнительную информацию'''
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Все члены семьи"
+        return context
 
 
-@login_required(login_url='users:user_login')
-def person_create_view(request):
-    if request.method == 'POST':
-        form = PersonForm(request.POST, request.FILES)
-        if form.is_valid():
-            person_object = form.save()
-            person_object.creator = request.user
-            person_object.save()
-            return HttpResponseRedirect(reverse('family_tree:persons'))
-    context = {
-        'title': 'Добавить члена семьи',
-        'form': PersonForm
-    }
-    return render(request, 'family_tree/person_create_update.html', context=context)
+class PersonCreateView(LoginRequiredMixin, CreateView):
+    '''Представление-класс для создания нового члена семьи.'''
+    model = Person
+    form_class = PersonForm
+    template_name = 'family_tree/person_create_update.html'
+    success_url = reverse_lazy('family_tree:persons')
+    login_url = 'users:user_login'
+
+    def form_valid(self, form):
+        '''Переопределяем поведение при успешной валидации формы.'''
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        """Добавляем заголовок страницы в контекст шаблона."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Добавить члена семьи"
+        return context
 
 
-def person_detail_view(request, pk):
-    person_object = Person.objects.get(pk=pk)
-    context = {
-        'person': person_object,
-        'title': person_object
-    }
-    return render(request, 'family_tree/person_detail.html', context=context)
+class PersonDetailView(DetailView):
+    '''Представление-класс для отображения подробной информации о человеке'''
+    model = Person
+    template_name = 'family_tree/person_detail.html'
+    context_object_name = 'person'
+
+    def get_context_data(self, **kwargs):
+        '''Добавляем заголовка в шаблон.'''
+
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object
+        return context
 
 
-@login_required(login_url='users:user_login')
-def person_update_view(request, pk):
-    person_object = get_object_or_404(Person, pk=pk)
-    if request.method == 'POST':
-        form = PersonForm(request.POST, request.FILES, instance=person_object)
-        if form.is_valid():
-            person_object = form.save()
-            person_object.save()
-            return HttpResponseRedirect(reverse('family_tree:person_detail', args={pk: pk}))
-    context = {
-        'object': person_object,
-        'title': 'Редактировать данные',
-        'form': PersonForm(instance=person_object)
-    }
-    return render(request, 'family_tree/person_create_update.html', context=context)
+class PersonUpdateView(LoginRequiredMixin, UpdateView):
+    '''Представление-класс для редактирования данных о человеке.'''
+    model = Person
+    form_class = PersonForm
+    template_name = 'family_tree/person_create_update.html'
+    context_object_name = 'object'
+    login_url = 'users:user_login'
 
-@login_required(login_url='users:user_login')
-def person_delete_view(request, pk):
-    person_object = get_object_or_404(Person, pk=pk)
-    if request.method == 'POST':
-        person_object.delete()
-        return HttpResponseRedirect(reverse('family_tree:persons'))
-    context = {
-        'object': person_object,
-        'title': "Удалить члена семьи"
-    }
-    return render(request, 'family_tree/person_delete.html', context=context)
+    def get_context_data(self, **kwargs):
+        """Добавляем заголовок в контекст шаблона."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактировать данные'
+        return context
+
+    def get_success_url(self):
+        """После успешного сохранения — перейти на страницу детального просмотра."""
+        return reverse('family_tree:person_detail', args=[self.object.pk])
 
 
-
-def tree_view(request, person_id=None):
-    if person_id is None:
-        person_id = 12  # корень дерева по умолчанию
-    root_person = get_object_or_404(Person, pk=person_id)
-    return render(request, 'family_tree/tree.html', {'root_person': root_person})
-
-def tree_data_view(request, person_id):
-    try:
-        root = Person.objects.get(pk=person_id)
-    except Person.DoesNotExist:
-        return JsonResponse({"error": "Person not found"}, status=404)
-
-    data = build_descendants_tree(root, is_spouse=False)
-    return JsonResponse(data)
+class PersonDeleteView(LoginRequiredMixin, DeleteView):
+    """Представление-класс для удаления объекта Person с подтверждением."""
+    model = Person
+    template_name = 'family_tree/person_delete.html'
+    context_object_name = 'object'
+    success_url = reverse_lazy('family_tree:persons')
+    login_url = 'users:user_login'
 
 
-def spouse_tree_data(request, spouse_id):
-    try:
-        spouse = Person.objects.get(pk=spouse_id)
-    except Person.DoesNotExist:
-        return JsonResponse({"error": "Spouse not found"}, status=404)
-    data = build_ancestors_tree(spouse)
-    return JsonResponse(data)
+class TreeView(DetailView):
+    """Класс-представление, показывающее генеалогическое дерево
+    с корнем в выбранном человеке."""
+    model = Person
+    template_name = 'family_tree/tree.html'
+    context_object_name = 'root_person'
+
+    def get_object(self, queryset=None):
+        """Переопределяем метод, чтобы задать person_id по умолчанию"""
+        pk = self.kwargs.get('person_id')
+        if pk is None:
+            pk = 12
+        return get_object_or_404(Person, pk=pk)
 
 
+class TreeDataView(APIView):
+    """API для получения данных генеалогического дерева потомков человека."""
 
-def build_descendants_tree(person, is_spouse=False):
-    spouse = person.spouse
-    pair_html = render_to_string('family_tree/includes/inc_card_pair.html', {
-        'person': person,
-        'spouse': spouse if not is_spouse else None,  # Если это супруг, не показываем супруга (чтобы не зациклить)
-    })
+    def get(self, request, person_id):
+        """Это метод, который отвечает на GET-запрос."""
+        root = get_object_or_404(Person, pk=person_id)
+        data = self.build_descendants_tree(root, is_spouse=False)
+        return Response(data, status=status.HTTP_200_OK)
 
-    children_nodes = []
-    # Если это супруг — не строим детей
-    if not is_spouse:
-        for child in person.children():
-            children_nodes.append(build_descendants_tree(child))
+    def build_descendants_tree(self, person, is_spouse=False):
+        """Метод строит дерево потомков для конкретного человека person."""
+        spouse = person.spouse
+        pair_html = render_to_string('family_tree/includes/inc_card_pair.html', {
+            'person': person,
+            'spouse': spouse if not is_spouse else None,
+        })
 
-    return {
-        "innerHTML": pair_html,
-        "children": children_nodes,
-        "person_id": person.id,
-        "spouse_id": spouse.id if spouse else None
-    }
+        children_nodes = []
+        if not is_spouse:
+            for child in person.children():
+                children_nodes.append(self.build_descendants_tree(child))
+
+        return {
+            'innerHTML': pair_html,
+            'children': children_nodes,
+            'person_id': person.id,
+            'spouse_id': spouse.id if spouse else None,
+        }
 
 
-def build_ancestors_tree(person):
-    # Рендерим карточку человека и его супруга(и)
-    pair_html = render_to_string('family_tree/includes/inc_card_pair.html', {
-        'person': person,
-        'spouse': person.spouse,  # Супруг(а) отображается, но его родители - нет
-    })
+class SpouseTreeDataView(APIView):
+    """API для получения данных генеалогического дерева предков (родителей) супруга/супруги."""
 
-    parents_nodes = []
+    def get(self, request, spouse_id):
+        """Это метод, который отвечает на GET-запрос."""
+        spouse = get_object_or_404(Person, pk=spouse_id)
+        data = self.build_ancestors_tree(spouse)
+        return Response(data, status=status.HTTP_200_OK)
 
-    # Добавляем только родителей текущего человека (жены)
-    if person.father:
-        parents_nodes.append(build_ancestors_tree(person.father))
+    def build_ancestors_tree(self, person):
+        """Функция строит родословную (только вверх: от человека к его родителям)"""
+        pair_html = render_to_string('family_tree/includes/inc_card_pair.html', {
+            'person': person,
+            'spouse': person.spouse,
+        })
 
-    if person.mother:
-        parents_nodes.append(build_ancestors_tree(person.mother))
+        parents_nodes = []
+        if person.father and person.mother:
+            parents_nodes.append(self.build_ancestors_tree(person.father))
+        elif person.father:
+            parents_nodes.append(self.build_ancestors_tree(person.father))
+        elif person.mother:
+            parents_nodes.append(self.build_ancestors_tree(person.mother))
 
-    return {
-        "innerHTML": pair_html,
-        "children": parents_nodes,
-        "person_id": person.id,
-        "spouse_id": person.spouse.id if person.spouse else None
-    }
-
+        return {
+            'innerHTML': pair_html,
+            'children': parents_nodes,
+            'person_id': person.id,
+            'spouse_id': person.spouse.id if person.spouse else None,
+        }
 
