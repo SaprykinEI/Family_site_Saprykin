f99fae3be7b5e92e0b7ced4e67e540caef75fc8c
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-17 20:26:21 +0300

    1. Создан шаблон и подключена библиотека JS FullCalendar

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from events.views import EventListView
-
+from events.views import EventListView, EventJsonView
 
 app_name = 'events'
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events_list'),
+    path('api/events/', EventJsonView.as_view(), name='events_json'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,10 +1,29 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, View
+from django.http import JsonResponse
+from django.utils.dateparse import parse_date
 
 from events.models import Event
 
 
 class EventListView(ListView):
     model = Event
-    template_name = 'events/event_list.html'
+    template_name = 'events/calendar.html'
     context_object_name = 'events'
-    
+
+
+class EventJsonView(View):
+    def get(self, request, *args, **kwargs):
+        events = Event.objects.all()
+
+        # Формируем список событий для FullCalendar
+        events_list = []
+        for event in events:
+            events_list.append({
+                "id": event.id,
+                "title": event.title,
+                "start": event.date.isoformat(),  # дата начала
+                # если нужна end_date, можно добавить "end": event.end_date.isoformat()
+                "allDay": True,
+                # Можно добавить дополнительные поля при необходимости
+            })
+        return JsonResponse(events_list, safe=False)
