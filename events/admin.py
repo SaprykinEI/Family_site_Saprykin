5fbee47d9add0fa3d5cca7d2273c2dc88e8e3064
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-17 18:46:44 +0300

    1. Зарегистрировали модель в admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'event_type')
+    list_filter = ('date', 'event_type', 'categories')
+    search_fields = ('title', 'event_type')
+    filter_horizontal = ('people', 'categories')
+    date_hierarchy = 'date'
+    ordering = ("-date",)
