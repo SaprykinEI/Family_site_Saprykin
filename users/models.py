a2583cfa0019763a1e798852f881b063b92a9907
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-24 07:53:44 +0300

    1. В модель добавлены роли

--- users/models.py
+++ users/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin',_('admin')
+    MODERATOR = 'moderator',_('moderator')
+    USER = 'user',_('user')
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     first_name = models.CharField(max_length=150, verbose_name="Имя", default="Анонимный")
     last_name = models.CharField(max_length=150, verbose_name="Фамилия", default="Пользователь")
     phone = models.CharField(max_length=20, verbose_name="Телефон", **NULLABLE)
