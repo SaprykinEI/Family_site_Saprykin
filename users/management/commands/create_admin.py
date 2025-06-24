202fd70fa8b74571ace49c38c5941406473d5645
SaprykinEI <saprykin.ei@yandex.ru>
2025-06-24 08:15:53 +0300

    1. Досоздал команду на создание Модератора и Юзера 2. Добавлены поля в админку

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -1,21 +1,54 @@
 from django.core.management import BaseCommand
 
-from users.models import User
+from users.models import User, UserRoles
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        admin_user = User.objects.create(
+        admin = User.objects.create(
             email='saprykin-family@yandex.com',
             first_name='Admin',
             last_name='Family-Site',
+            role=UserRoles.ADMIN,
             is_staff=True,
             is_superuser=True,
             is_active=True,
             is_verified=True,
         )
 
-        admin_user.set_password('saprykinfamilysite')
-        admin_user.save()
+        admin.set_password('saprykinfamilysite')
+        admin.save()
         print("Администратор создан")
+
+        moderator = User.objects.create(
+            email='moderator@yandex.com',
+            first_name='Moderator',
+            last_name='Moderator',
+            role=UserRoles.MODERATOR,
+            is_staff=True,
+            is_superuser=False,
+            is_active=True,
+            is_verified=True,
+        )
+
+        moderator.set_password('saprykinfamilysite')
+        moderator.save()
+        print("Модератор создан")
+
+        user = User.objects.create(
+            email='saprykin-family@yandex.com',
+            first_name='User',
+            last_name='User',
+            role=UserRoles.USER,
+            is_staff=False,
+            is_superuser=True,
+            is_active=True,
+            is_verified=True,
+        )
+
+        user.set_password('saprykinfamilysite')
+        user.save()
+        print("Юзер создан")
+
+
